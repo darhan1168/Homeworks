cbc688a0f1bc08240e3dade7a514cbd5e235eb4f
darhan1168 <darinahanina04@gmail.com>
2023-03-20 16:57:48 +0100

    Create class Project
    with fields Name, Description, Deadline, Budget

---
+++ Homework2/Homework2/Project.cs
@@ -0,0 +1,61 @@
+namespace Homework2;
+
+public class Project
+{
+    private string _name;
+    private string _description;
+    private string _deadline;
+    private decimal _budget;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (int.TryParse(value, out _))
+                throw new AggregateException("Name is num");
+            _name = value;
+        }
+    }
+
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (int.TryParse(value, out _))
+                throw new AggregateException("Description is num");
+            _description = value;
+        }
+    }
+
+    public string Deadline
+    {
+        get => _deadline;
+        set
+        {
+            if (int.TryParse(value, out _))
+                throw new AggregateException("Deadline is num");
+            _deadline = value;
+        }
+    }
+
+    public decimal Budget
+    {
+        get => _budget;
+        set
+        {
+            if (value < 0 || value > 1_000_000)
+                throw new ArgumentOutOfRangeException($"Budget isn`t rahge");
+            _budget = value;
+        }
+    }
+
+    public Project(string name, string description, string deadline, decimal budget)
+    {
+        Name = name;
+        Description = description;
+        Deadline = deadline;
+        Budget = budget;
+    }
+}
