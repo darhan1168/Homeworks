ac9e696b0b865d2b54865dd571ba9266611a51cc
darhan1168 <darinahanina04@gmail.com>
2023-03-21 17:09:28 +0100

    Add methods in class Staff
    methods for creating, deleting, getting staff

--- Homework2/Homework2/ManagedStaff.cs
+++ Homework2/Homework2/ManagedStaff.cs
@@ -8,4 +8,19 @@ public class ManagedStaff
     {
         _staff = new List<Staff>();
     }
+    
+    public void CreateStaff(Staff staff)
+    {
+        _staff.Add(staff);
+    }
+
+    public void DeleteStaff(int index)
+    {
+        _staff.RemoveAt(index);
+    }
+
+    public List<Staff> GetStaff()
+    {
+        return _staff;
+    }
 }
