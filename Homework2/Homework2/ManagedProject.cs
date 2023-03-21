828f61e5224f103f537d48994dabb9d46487f7cd
darhan1168 <darinahanina04@gmail.com>
2023-03-21 16:43:20 +0100

    Create class for project management

---
+++ Homework2/Homework2/ManagedProject.cs
@@ -0,0 +1,11 @@
+namespace Homework2;
+
+public class ManagedProject
+{
+    private List<Project> _projects;
+
+    public ManagedProject()
+    {
+        _projects = new List<Project>();
+    }
+}
