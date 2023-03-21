e4d2e599eb9709a7ff4ef59a0cd9a39d52a8e782
darhan1168 <darinahanina04@gmail.com>
2023-03-21 17:08:00 +0100

    Add methods in class Project
    methods for creating, deleting, getting projects

--- Homework2/Homework2/ManagedProject.cs
+++ Homework2/Homework2/ManagedProject.cs
@@ -8,4 +8,19 @@ public class ManagedProject
     {
         _projects = new List<Project>();
     }
+
+    public void CreateProject(Project project)
+    {
+        _projects.Add(project);
+    }
+
+    public void DeleteProject(int index)
+    {
+        _projects.RemoveAt(index);
+    }
+
+    public List<Project> GetProjects()
+    {
+        return _projects;
+    }
 }
