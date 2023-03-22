6c748cfb4daf3fc1d490d55a697f886ddc155064
darhan1168 <darinahanina04@gmail.com>
2023-03-22 10:45:17 +0100

    Update ManagedProject.cs
    Add method in class ManagedProject for showing list of projects

--- Homework2/Homework2/ManagedProject.cs
+++ Homework2/Homework2/ManagedProject.cs
@@ -19,6 +19,20 @@ public class ManagedProject
         _projects.RemoveAt(index);
     }
 
+    public void ShowProject()
+    {
+        int index = 1;
+        
+        if (_projects.Count == 0)
+            Console.WriteLine("Project not added yet");
+        
+        foreach (var project in _projects)
+        {
+            Console.WriteLine($"{index} - Title: {project.Name}, Description: {project.Description}, Deadline: {project.Deadline}, Budget: {project.Budget}");
+            index++;
+        }
+    }
+    
     public List<Project> GetProjects()
     {
         return _projects;
