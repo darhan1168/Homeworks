36dec76894624b9207190d90c1af144e31354e20
darhan1168 <darinahanina04@gmail.com>
2023-03-22 12:00:28 +0100

    Create method ConsoleApp in class Program
    where we process data in cycle while by switch case

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -4,6 +4,37 @@
     {
         public static void Main(string[] args)
         {
+        public static void ConsoleApp()
+        {
+            ManagedProject managedProject = new ManagedProject();
+            ManagedTasks managedTasks = new ManagedTasks();
+            ManagedStaff managedStaff = new ManagedStaff();
+            ConsoleRobot consoleRobot = new ConsoleRobot();
+
+            while (true)
+            {
+                consoleRobot.WriteMassage("Choose what you want (1 - Project status review, 2 - Addition of options, 3 - Deletion of options, 4 - Replacement of options, 5 - Watching status of options,  6 - Stop console app)");
+                string? answer = consoleRobot.ReadInput("Answer:");
+
+                switch (answer)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        return;
+                    default:
+                        consoleRobot.WriteMassage("- Your answer should be 1 to 6,  try again");
+                        break;
+                }
+            }
         }
     }
 }
