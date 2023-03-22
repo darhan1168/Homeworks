1825a05e98eafbea0be9063f79e4f6f9fa5958ff
darhan1168 <darinahanina04@gmail.com>
2023-03-22 13:50:14 +0100

    Delete class consoleAdding
    because data was not in memory

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -10,7 +10,6 @@
             ManagedTasks managedTasks = new ManagedTasks();
             ManagedStaff managedStaff = new ManagedStaff();
             ConsoleRobot consoleRobot = new ConsoleRobot();
-            ConsoleAdding consoleAdding = new ConsoleAdding();
 
             while (true)
             {
@@ -28,13 +27,61 @@
                         switch (answerUserAdd)
                         {
                             case "1":
-                                consoleAdding.AddProject();
+                                consoleRobot.WriteMassage("Enter name of project");
+                                string? newNameProject = consoleRobot.ReadInput();
+
+                                consoleRobot.WriteMassage("Enter description of project");
+                                string? newDescriptionProject = consoleRobot.ReadInput();
+                                
+                                consoleRobot.WriteMassage("Enter deadline of project");
+                                string? newDeadlineProject = consoleRobot.ReadInput();
+                                
+                                consoleRobot.WriteMassage("Enter budget of project");
+                                decimal newBudgetProject = decimal.Parse(consoleRobot.ReadInput() ?? string.Empty);
+
+                                if (newNameProject != null && newDescriptionProject != null && newDeadlineProject != null)
+                                {
+                                    Project newProject = new Project(newNameProject, newDescriptionProject, newDeadlineProject, newBudgetProject);
+                                    managedProject.CreateProject(newProject);
+                                    consoleRobot.WriteMassage("Your new project added");
+                                    managedProject.ShowProject();
+                                }
+                                else
+                                    throw new ArgumentNullException();
+                                
                                 break;
                             case "2":
-                                consoleAdding.AddTask();
+                                consoleRobot.WriteMassage("Enter description of task");
+                                string? newDescriptionTask = consoleRobot.ReadInput();
+
+                                if (newDescriptionTask != null)
+                                {
+                                    Tasks tasks = new Tasks(newDescriptionTask);
+                                    managedTasks.CreateTasks(tasks);
+                                    consoleRobot.WriteMassage("Your new task added");
+                                    managedTasks.ShowTasks();
+                                }
+                                else
+                                    throw new ArgumentNullException();
+                                
                                 break;
                             case "3":
-                                consoleAdding.AddStaff();
+                                consoleRobot.WriteMassage("Enter name of person");
+                                string? newNameStaff = consoleRobot.ReadInput();
+                                
+                                consoleRobot.WriteMassage("Enter post of person");
+                                string? newPostStaff = consoleRobot.ReadInput();
+
+                                if (newNameStaff != null && newPostStaff != null)
+                                {
+                                    Staff staff = new Staff(newNameStaff, newPostStaff);
+                                    managedStaff.CreateStaff(staff);
+                                    consoleRobot.WriteMassage("Your new staff added");
+                                    managedStaff.ShowStaff();
+                                }
+                                else
+                                    throw new ArgumentNullException();
+                                
                                 break;
                             default:
                                 consoleRobot.WriteMassage("- Your answer should be 1 to 3,  try again");
