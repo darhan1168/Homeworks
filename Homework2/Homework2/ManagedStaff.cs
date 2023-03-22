2d19d2fd30974c82f56568ce5c6c8082fe5d1db1
darhan1168 <darinahanina04@gmail.com>
2023-03-22 10:45:52 +0100

    Update ManagedStaff.cs
    Add method in class ManagedStaff for showing list of staff

--- Homework2/Homework2/ManagedStaff.cs
+++ Homework2/Homework2/ManagedStaff.cs
@@ -18,6 +18,20 @@ public class ManagedStaff
     {
         _staff.RemoveAt(index);
     }
+    
+    public void ShowStaff()
+    {
+        int index = 1;
+        
+        if (_staff.Count == 0)
+            Console.WriteLine("Staff not added yet");
+        
+        foreach (var staff in _staff)
+        {
+            Console.WriteLine($"{index} - Name: {staff.Name}, Post: {staff.Post}");
+            index++;
+        }
+    }
 
     public List<Staff> GetStaff()
     {
