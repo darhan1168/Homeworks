a80e4d4b036766bc2759da9ff20afd8b5df7dbfc
darhan1168 <darinahanina04@gmail.com>
2023-03-22 12:48:41 +0100

    Change answer on answerUserMethod

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -15,8 +15,10 @@
             {
                 consoleRobot.WriteMassage("Choose what you want (1 - Project status review, 2 - Addition of options, 3 - Deletion of options, 4 - Replacement of options, 5 - Watching status of options,  6 - Stop console app)");
                 string? answer = consoleRobot.ReadInput("Answer:");
+                string? answerUserMethod = consoleRobot.ReadInput();
 
                 switch (answer)
+                switch (answerUserMethod)
                 {
                     case "1":
                         break;
