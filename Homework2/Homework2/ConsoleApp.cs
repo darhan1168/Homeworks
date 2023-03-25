35722d1dc76e321ab589dc17b750abb9563cbfd3
darhan1168 <darinahanina04@gmail.com>
2023-03-25 17:49:50 +0100

    Create new class ConsoleAppForDeletion
    for deleting project, task, staff

--- Homework2/Homework2/ConsoleApp.cs
+++ Homework2/Homework2/ConsoleApp.cs
@@ -32,43 +32,9 @@ public class ConsoleApp
                         consoleAppForAdding.ConsoleForAdding();
                         break;
                     case "3":
-                        Console.WriteLine("Choose what you want delete (1 - project, 2 - task, 3 - staff)");
-                        string? answerUserDelete = consoleRobot.ReadInput();
-
-                        switch (answerUserDelete)
-                        {
-                            case "1":
-                                Console.WriteLine("Enter the project sequence number");
-                                int sequenceNumberProject = int.Parse(consoleRobot.ReadInput() ?? string.Empty);
-            
-                                managedProject.DeleteProject(sequenceNumberProject - 1);
-                                Console.WriteLine($"Project {sequenceNumberProject} deleted");
-                                managedProject.ShowProject();
-                                
-                                break;
-                            case "2":
-                                Console.WriteLine("Enter the task sequence number");
-                                int sequenceNumberTask = int.Parse(consoleRobot.ReadInput() ?? string.Empty);
-            
-                                managedTasks.DeleteTasks(sequenceNumberTask - 1);
-                                Console.WriteLine($"Task {sequenceNumberTask} deleted");
-                                managedTasks.ShowTasks();
-
-                                break;
-                            case "3":
-                                Console.WriteLine("Enter the staff sequence number");
-                                int sequenceNumberStaff = int.Parse(consoleRobot.ReadInput() ?? string.Empty);
-                                
-                                managedStaff.DeleteStaff(sequenceNumberStaff - 1);
-                                Console.WriteLine($"Staff {sequenceNumberStaff} deleted");
-                                managedStaff.ShowStaff();
-                                
-                                break;
-                            default:
-                                Console.WriteLine("- Your answer should be 1 to 3,  try again");
-                                break;
-                        }
+                        ConsoleAppForDeletion consoleAppForDeletion = new ConsoleAppForDeletion();
                         
+                        consoleAppForDeletion.ConsoleForDeletion();
                         break;
                     case "4":
                         Console.WriteLine("Choose where you want replace options ((1 - project, 2 - task, 3 - staff)");
