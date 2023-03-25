6d4059191b25230cdfbb409f2c5d74896ece8379
darhan1168 <darinahanina04@gmail.com>
2023-03-25 18:01:33 +0100

    Create new class ConsoleAppForReplacing
    for replacing project, task, staff

--- Homework2/Homework2/ConsoleApp.cs
+++ Homework2/Homework2/ConsoleApp.cs
@@ -37,81 +37,9 @@ public class ConsoleApp
                         consoleAppForDeletion.ConsoleForDeletion();
                         break;
                     case "4":
-                        Console.WriteLine("Choose where you want replace options ((1 - project, 2 - task, 3 - staff)");
-                        string? answerUserReplace = consoleRobot.ReadInput();
-
-                        switch (answerUserReplace)
-                        {
-                            case "1":
-                                while (true)
-                                {
-                                    Console.WriteLine("Choose which options you need to replace ((1 - name, 2 - description, 3 - deadline, 4 - budget, 5 - stop choosing)");
-                                    string? answerWhatReplaceInProject = consoleRobot.ReadInput();
-                                    
-                                    if (answerWhatReplaceInProject == "5")
-                                        break;
-                                    
-                                    switch (answerWhatReplaceInProject)
-                                    {
-                                        case "1":
-                                            managedProject.ReplaceName(consoleRobot.GetNewValue(), consoleRobot.GetSequenceNumber());
-                                            break;
-                                        case "2":
-                                            managedProject.ReplaceDescription(consoleRobot.GetNewValue(), consoleRobot.GetSequenceNumber());
-                                            break;
-                                        case "3":
-                                            managedProject.ReplaceDeadline(consoleRobot.GetNewValue(), consoleRobot.GetSequenceNumber());
-                                            break;
-                                        case "4":
-                                            managedProject.ReplaceBudget(int.Parse(consoleRobot.GetNewValue()), consoleRobot.GetSequenceNumber());
-                                            break;
-                                        default:
-                                            Console.WriteLine("- Your answer should be 1 to 3, try again");
-                                            break;
-                                    }
-                                    
-                                    managedProject.ShowProject();
-                                }
-
-                                break;
-                            case "2":
-                                Console.WriteLine("Enter a new value and sequence number");
-                                
-                                managedTasks.ReplaceDescription(consoleRobot.GetNewValue(), consoleRobot.GetSequenceNumber());
-                                managedTasks.ShowTasks();
-
-                                break;
-                            case "3":
-                                while (true)
-                                {
-                                    Console.WriteLine("Choose which options you need to replace ((1 - name, 2 - post, 3 - stop choosing)");
-                                    string? answerWhatReplaceInStaff = consoleRobot.ReadInput();
-
-                                    if (answerWhatReplaceInStaff == "3")
-                                        break;
-
-                                    switch (answerWhatReplaceInStaff)
-                                    {
-                                        case "1":
-                                            managedStaff.ReplaceName(consoleRobot.GetNewValue(), consoleRobot.GetSequenceNumber());
-                                            break;
-                                        case "2":
-                                            managedStaff.ReplacePost(consoleRobot.GetNewValue(), consoleRobot.GetSequenceNumber());
-                                            break;
-                                        default:
-                                            Console.WriteLine("- Your answer should be 1 to 3, try again");
-                                            break;
-                                    }
-                                    
-                                    managedStaff.ShowStaff();
-                                }
-
-                                break;
-                            default:
-                                Console.WriteLine("- Your answer should be 1 to 3, try again");
-                                break;
-                        }
+                        ConsoleAppForReplacing consoleAppForReplacing = new ConsoleAppForReplacing();
                         
+                        consoleAppForReplacing.ConsoleForReplacing();
                         break;
                     case "5":
                         return;
