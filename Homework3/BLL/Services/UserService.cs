3a9d7d619ca21529fc721dc57dcb66c693d261ae
darhan1168 <darinahanina04@gmail.com>
2023-04-07 16:57:12 +0200

    Do GetUsersByRole

--- Homework3/BLL/Services/UserService.cs
+++ Homework3/BLL/Services/UserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BLL.Abstractions.Interfaces;
+using Core.Enums;
 using Core.Models;
 using DAL.Abstractions.Interfaces;
 
@@ -68,7 +69,38 @@ namespace BLL.Services
 
         public async Task<List<User>> GetUsersByRole(string role)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var users = await GetAll();
+
+                if (users is null)
+                {
+                    throw new Exception("Users are null");
+                }
+
+                UserRole avaibleRole;
+
+                switch (role)
+                {
+                    case "Admin":
+                        avaibleRole = UserRole.Admin;
+                        break;
+                    case "Trainer":
+                        avaibleRole = UserRole.Trainer;
+                        break;
+                    case "Member":
+                        avaibleRole = UserRole.Member;
+                        break;
+                    default:
+                        throw new Exception("Incorrect role");
+                }
+
+                return users.Where(u => u.Role == avaibleRole).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to get user by role {role}. Exception: {ex.Message}");
+            }
         }
 
         public async Task UpdatePassword(Guid userId, string newPassword)
