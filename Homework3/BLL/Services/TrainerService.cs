d4cf1267eb62debda8495f8e32f45b628ff0b9d3
darhan1168 <darinahanina04@gmail.com>
2023-04-07 14:48:05 +0200

    Created an implementation in TrainerService

--- Homework3/BLL/Services/TrainerService.cs
+++ Homework3/BLL/Services/TrainerService.cs
@@ -22,8 +22,21 @@ namespace BLL.Services
 
         public async Task<Trainer> AddTrainer(Trainer trainer)
         {
-            await Add(trainer);
-            return trainer;
+            try
+            {
+                await Add(trainer);
+
+                if (trainer is null)
+                {
+                    throw new Exception("Trainer is null");
+                }
+                
+                return trainer;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to add trainer {trainer}. Exception: {ex.Message}");
+            }
         }
 
         public async Task<List<Trainer>> GetTrainersBySpecialization(string specialization)
@@ -31,7 +44,13 @@ namespace BLL.Services
             try
             {
                 var trainers = await GetAll();
-                return trainers.Where(t => t.Specialization.Equals(specialization, StringComparison.OrdinalIgnoreCase)).ToList();
+                
+                if (trainers is null)
+                {
+                    throw new Exception("Trainers are null");
+                }
+
+                return trainers.Where(t => t.Specialization == specialization).ToList();
             }
             catch (Exception ex)
             {
@@ -44,23 +63,17 @@ namespace BLL.Services
             try
             {
                 var trainers = await GetAll();
-                var unavailableTrainers = new List<Trainer>();
-                
-                foreach (var trainer in trainers)
+
+                if (trainers is null)
                 {
-                    var isAvailable = await CheckTrainerAvailability(trainer.Id, date);
-                    
-                    if (!isAvailable)
-                    {
-                        unavailableTrainers.Add(trainer);
-                    }
+                    throw new Exception("Trainers are null");
                 }
-                
-                return trainers.Except(unavailableTrainers).ToList();
+
+                return trainers.Where(t => t.AvailableDates.Contains(date)).ToList();
             }
             catch (Exception ex)
             {
-                throw new Exception($"Failed to get trainers by date {date}. Exception: {ex.Message}");
+                throw new Exception($"Failed to get trainers by date {date.ToShortDateString()}. Exception: {ex.Message}");
             }
         }
         
@@ -69,16 +82,18 @@ namespace BLL.Services
             try
             {
                 var trainer = await GetById(trainerId);
-
-                foreach (var availableDate in trainer.AvailableDates)
-                {
-                    if (availableDate == date)
-                    {
-                        return true;
-                    }
-                }
                 
-                return trainer.AvailableDates.All(c => c.Date != date);
+                if (trainer is null)
+                {
+                    throw new Exception("Trainer is null");
+                }
+
+                if (trainer.AvailableDates.Contains(date))
+                {
+                    return true;
+                }
+
+                return false;
             }
             catch (Exception ex)
             {
@@ -88,7 +103,32 @@ namespace BLL.Services
 
         public async Task AssignTrainerToClass(Guid trainerId, Guid classId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var trainer = await GetById(trainerId);
+                var fitClass = await _classService.GetById(classId);
+                
+                if (trainer is null)
+                {
+                    throw new Exception("Trainer is null");
+                }
+                
+                if (fitClass is null)
+                {
+                    throw new Exception("Class is null");
+                }
+
+                if (fitClass.Trainer == trainer)
+                {
+                    throw new Exception("Trainer already assigned to class");
+                }
+
+                fitClass.Trainer = trainer;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to check assign trainer {trainerId} to class {classId}. Exception: {ex.Message}");
+            }
         }
     }
 }
