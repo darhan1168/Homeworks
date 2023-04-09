8348528c4349a2685829864285cbbf9c20b2d016
darhan1168 <darinahanina04@gmail.com>
2023-04-09 11:28:54 +0200

    Create method for  adding new users

--- Homework3/UI/ConsoleManagers/UserConsoleManager.cs
+++ Homework3/UI/ConsoleManagers/UserConsoleManager.cs
@@ -58,7 +58,67 @@ namespace UI.ConsoleManagers
 
         public async Task CreateUserAsync()
         {
-            // Implementation for creating a new user
+            try
+            {
+                var users = await Service.GetAll();
+                
+                Console.WriteLine("Enter your Username");
+                var username = Console.ReadLine();
+                var userEqUsername = users.Where(u => u.Username == username).ToList();
+
+                if (userEqUsername.Count > 0)
+                {
+                    throw new Exception("Username already added");
+                }
+                
+                Console.WriteLine("Enter your Password or 1 - generate random");
+                var answerPassword = Console.ReadLine();
+                string password;
+                
+                if (answerPassword == "1")
+                {
+                    password = GeneratePassword();
+                    Console.WriteLine($"Your password: {password}");
+                }
+                else
+                {
+                    password = answerPassword;
+                }
+
+                Console.WriteLine("Enter your Role (1 - Admin, 2 - Trainer, 3 - Member)");
+                var answerRole = Console.ReadLine();
+                UserRole role;
+
+                if (answerRole == "1")
+                {
+                    role = UserRole.Admin;
+                }
+                else if (answerRole == "2")
+                {
+                    role = UserRole.Admin;
+                }
+                else if (answerRole == "3")
+                {
+                    role = UserRole.Admin;
+                }
+                else
+                {
+                    throw new Exception("Incorrect answer about role");
+                }
+                
+                await Service.Add(new User()
+                {
+                    Id = new Guid(),
+                    Username = username,
+                    PasswordHash = password,
+                    Role = role,
+                    IsLocked = false
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to create user. Exception: {ex.Message}");
+            }
         }
 
         public async Task UpdateUserAsync()
