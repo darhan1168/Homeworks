b2761aa7fa978306f5767d917bc954f5ec8fea2f
darhan1168 <darinahanina04@gmail.com>
2023-04-09 11:30:24 +0200

    Update UserConsoleManager.cs

--- Homework3/UI/ConsoleManagers/UserConsoleManager.cs
+++ Homework3/UI/ConsoleManagers/UserConsoleManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using BLL.Abstractions.Interfaces;
+using Core.Enums;
 using Core.Models;
 using UI.Interfaces;
 
