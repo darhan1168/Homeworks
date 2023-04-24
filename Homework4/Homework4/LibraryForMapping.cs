741f3efd8c21c05483018ffc841945349bf6b745
darhan1168 <darinahanina04@gmail.com>
2023-04-24 15:18:12 +0200

    Created method for checking properties too

--- Homework4/Homework4/LibraryForMapping.cs
+++ Homework4/Homework4/LibraryForMapping.cs
@@ -16,4 +16,27 @@ public static class LibraryForMapping
             }
         }
     }
+
+    public static void MapToSpecificProperties<TSource, TDestination>(this TSource source, TDestination destination,
+        string[] sourceProperties, string[] destinationProperties)
+    {
+        foreach (var sourceProperty in sourceProperties)
+        {
+            var sourceProp = typeof(TSource).GetProperty(sourceProperty);
+            if (sourceProp == null)
+            {
+                continue;
+            }
+
+            var destinationProperty = destinationProperties.FirstOrDefault(x => x == sourceProperty);
+            if (destinationProperty != null)
+            {
+                var destinationProp = typeof(TDestination).GetProperty(destinationProperty);
+                if (destinationProp != null)
+                {
+                    destinationProp.SetValue(destination, sourceProp.GetValue(source));
+                }
+            }
+        }
+    }
 }
